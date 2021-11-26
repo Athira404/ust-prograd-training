fa73835da3e0380ac0a8c60bdb9494e59d0588b3
Athira404 <athira7569@gmail.com>
2021-11-26 08:05:49 +0530

    Get Set moethods

---
+++ Day 4/CubeGetSet.java
@@ -0,0 +1,11 @@
+public class Cube{
+    // note: use primitive data type for the side of the cube
+  private int Side=0;
+  public int getSide() {
+      return Side; 
+  }
+
+  public void setSide(int side) {
+      Side = side; 
+  }
+}
