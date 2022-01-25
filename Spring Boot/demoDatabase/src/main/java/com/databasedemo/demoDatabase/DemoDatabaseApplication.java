c4bcefa146e0370de2f4e9370cff1c218fb0cd59
Athira404 <athira7569@gmail.com>
2022-01-25 13:15:54 +0530

    Demo database app for spring boot

---
+++ Spring Boot/demoDatabase/src/main/java/com/databasedemo/demoDatabase/DemoDatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.databasedemo.demoDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDatabaseApplication.class, args);
+	}
+}
+
+
