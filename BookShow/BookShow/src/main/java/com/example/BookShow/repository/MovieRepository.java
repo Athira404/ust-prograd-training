86cbe3da51de993767e5b347cae12f55c8b50b2e
Athira404 <94703116+Athira404@users.noreply.github.com>
2022-01-25 13:35:23 +0530

    Delete BookShow directory

--- BookShow/BookShow/src/main/java/com/example/BookShow/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.BookShow.repository;
-
-import com.example.BookShow.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-
-}
--- BookShow/BookShow/src/main/java/com/example/BookShow/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.BookShow.repository;
-
-import com.example.BookShow.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface UserRepository extends JpaRepository<User, String > {
-}
